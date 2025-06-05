168ea51eac48afa426f825880e61dce47c3b8b44
MICHAEL DESMOND <mdesmond@us.ibm.com>
2025-06-05 14:41:03 -0400

    fix: improve llms incorporation of thoughts
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

--- granite_chat/thinking/prompts.py
+++ granite_chat/thinking/prompts.py
@@ -5,18 +5,18 @@ class ThinkingPrompts:
     @staticmethod
     def thinking_system_prompt() -> str:
         return """
-Write down your thoughts and reasoning process as if you're thinking out loud before responding.
-Do not provide a response, you will do that later. For not just think through the response.
+Write down your thoughts and reasoning process as if you're thinking out loud before responding. Think step by step.
+Do NOT provide a response, you will do that later.
 """
 
     @staticmethod
     def responding_system_prompt(thoughts: str) -> str:
         return f"""
-Respond in a comprehensive and detailed way.
-Use your thoughts to guide and inform your response.
+You are a helpful assistant tasked with generating a comprehensive, informative, accurate, and easy-to-read response.
+IMPORTANT: Use your previous thoughts to guide and inform your response.
 
 Here are your thoughts:
 {thoughts}
 
-
+If you do not incorporate your thoughts, the response will be considered incomplete.
 """
