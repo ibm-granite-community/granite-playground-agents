e2e038d97bf2067ec1c5607949f7d0dfe223bfc9
MICHAEL DESMOND <mdesmond@us.ibm.com>
2025-06-16 22:18:32 -0400

    fix: search optimizations
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

--- granite_chat/search/vector_store.py
+++ granite_chat/search/vector_store.py
@@ -4,14 +4,15 @@ Enables configurable chunk size
 Add document index
 """
 
-from gpt_researcher.vector_store import VectorStoreWrapper  # type: ignore
+from typing import Any
+
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import VectorStore
 from transformers import AutoTokenizer
 
 
-class ConfigurableVectorStoreWrapper(VectorStoreWrapper):
+class ConfigurableVectorStoreWrapper:
     def __init__(
         self,
         vector_store: VectorStore,
@@ -19,10 +20,19 @@ class ConfigurableVectorStoreWrapper(VectorStoreWrapper):
         chunk_overlap: int = 200,
         tokenizer: AutoTokenizer | None = None,
     ) -> None:
-        super().__init__(vector_store)
+        self.vector_store = vector_store
         self.chunk_size = chunk_size
         self.chunk_overlap = chunk_overlap
         self.tokenizer = tokenizer
+
+    def load(self, documents: list[dict[str, str]]) -> None:
+        """
+        Load the documents into vector_store
+        Translate to langchain doc type, split to chunks then load
+        """
+        langchain_documents = self._create_langchain_documents(documents)
+        splitted_documents = self._split_documents(langchain_documents)
+        self.vector_store.add_documents(splitted_documents)
 
     def _create_langchain_documents(self, data: list[dict]) -> list[Document]:
         return [
@@ -30,9 +40,7 @@ class ConfigurableVectorStoreWrapper(VectorStoreWrapper):
             for i, item in enumerate(data)
         ]
 
-    def _split_documents(
-        self, documents: list[Document], chunk_size: int = 1000, chunk_overlap: int = 200
-    ) -> list[Document]:
+    def _split_documents(self, documents: list[Document]) -> list[Document]:
         """
         Split documents into smaller chunks
         """
@@ -49,3 +57,8 @@ class ConfigurableVectorStoreWrapper(VectorStoreWrapper):
             )
 
         return text_splitter.split_documents(documents)
+
+    async def asimilarity_search(self, query: str, k: int, filter: dict[str, Any] | None = None) -> list[Document]:
+        """Return query by vector store"""
+        results = await self.vector_store.asimilarity_search(query=query, k=k, filter=filter)
+        return results
