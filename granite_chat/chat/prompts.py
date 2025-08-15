e54f58f225687fdf5831178f3f0efb5d5e7ebf5f
MICHAEL DESMOND <mdesmond@us.ibm.com>
2025-08-15 15:26:43 -0400

    fix: chat system prompt, better report headings
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

--- granite_chat/chat/prompts.py
+++ granite_chat/chat/prompts.py
@@ -4,4 +4,12 @@ class ChatPrompts:
 
     @staticmethod
     def chat_system_prompt() -> str:
-        return """You are Granite, an AI language model developed by IBM."""
+        return f"""You are Granite, an AI language model developed by IBM.
+{ChatPrompts.chat_core_guidelines()}"""
+
+    @staticmethod
+    def chat_core_guidelines() -> str:
+        return """Always format your responses using Markdown, no LaTeX formatting.
+Do not give advice that could be unsafe or unethical.
+When appropriate, suggest follow-up questions to continue the conversation or explore the topic further.
+"""
--- granite_chat/thinking/prompts.py
+++ granite_chat/thinking/prompts.py
@@ -3,25 +3,6 @@ class ThinkingPrompts:
         pass
 
     @staticmethod
-    def thinking_system_prompt() -> str:
-        return """
-Write down your thoughts and reasoning process as if you're thinking out loud before responding. Think step by step.
-Do NOT provide a response, you will do that later.
-"""
-
-    @staticmethod
-    def responding_system_prompt(thoughts: str) -> str:
-        return f"""
-You are a helpful assistant tasked with generating a comprehensive, informative, accurate, and easy-to-read response.
-IMPORTANT: Use your previous thoughts to guide and inform your response.
-
-Here are your thoughts:
-{thoughts}
-
-If you do not incorporate your thoughts, the response will be considered incomplete.
-"""
-
-    @staticmethod
     def granite3_3_thinking_system_prompt() -> str:
         return """
 Knowledge Cutoff Date: April 2024.\nYou are Granite, developed by IBM.
