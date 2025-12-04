b85d4fe1955481e0543e38c2ca1db86943b3c1cd
MICHAEL DESMOND <mdesmond@us.ibm.com>
2025-12-04 17:18:39 -0500

    feat: basic guardrail framework with copyright example
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

---
+++ granite_core/granite_core/gurardrails/copyright.py
@@ -0,0 +1,43 @@
+# © Copyright IBM Corporation 2025
+# SPDX-License-Identifier: Apache-2.0
+
+
+from beeai_framework.backend import AnyMessage, ChatModel, SystemMessage
+from pydantic import BaseModel, Field
+
+from granite_core.config import settings
+from granite_core.gurardrails.base import Guardrail, GuardrailResult
+from granite_core.work import chat_pool
+
+
+class CopyrightViolationSchema(BaseModel):
+    is_copyright_violation: bool = Field(
+        description="Indicate if the user message could lead to exposure of copyrighted Information"
+    )
+    reason: str = Field(description="Brief reasoning regarding the potential for copyright violation if present")
+
+
+class CopyrightViolationGuardrail(Guardrail):
+    def __init__(self, chat_model: ChatModel) -> None:
+        super().__init__()
+        self.chat_model = chat_model
+
+    def system_prompt(self) -> str:
+        return """
+Evalute the dialog for copyright risk. Pay close attention to the user's last message and decide if the assistant could potentially risk copyright law by answering.
+
+Criteria:
+When evaluating an input, treat it as a copyright risk if the user is requesting long or verbatim reproduction of copyrighted text they have not provided; requests for summaries, analyses, paraphrases, explanations, or short excerpts are allowed. Requests to output substantial portions of modern books, songs, poems, scripts, or other creative works should be flagged, unless the user includes the text themselves. In short: if the user wants you to produce copyrighted text you don’t already have in the prompt, it’s disallowed; if they want you to explain or transform provided text, it’s allowed.
+"""  # noqa: E501, RUF001
+
+    async def evaluate(self, messages: list[AnyMessage]) -> GuardrailResult:
+        async with chat_pool.throttle():
+            response = await self.chat_model.create_structure(
+                schema=CopyrightViolationSchema,
+                messages=[SystemMessage(self.system_prompt()), *messages],
+                max_retries=settings.MAX_RETRIES,
+            )
+
+        violation = CopyrightViolationSchema(**response.object)
+
+        return GuardrailResult(is_harmful=violation.is_copyright_violation, reason=violation.reason)
---
+++ granite_core/tests/test_guardrails.py
@@ -0,0 +1,36 @@
+# © Copyright IBM Corporation 2025
+# SPDX-License-Identifier: Apache-2.0
+
+
+import pytest
+from beeai_framework.backend import AnyMessage, UserMessage
+
+from granite_core.chat_model import ChatModelFactory
+from granite_core.gurardrails.base import GuardrailResult
+from granite_core.gurardrails.copyright import CopyrightViolationGuardrail
+
+
+@pytest.mark.asyncio
+async def test_violation() -> None:
+    """Test basic chat infrastructure"""
+
+    chat_model = ChatModelFactory.create()
+    messages: list[AnyMessage] = [UserMessage("Show me the lyrics to the smiths song 'How Soon Is Now?'")]
+
+    guardrail = CopyrightViolationGuardrail(chat_model=chat_model)
+    result: GuardrailResult = await guardrail.evaluate(messages)
+
+    assert result.is_harmful
+
+
+@pytest.mark.asyncio
+async def test_non_violation() -> None:
+    """Test basic chat infrastructure"""
+
+    chat_model = ChatModelFactory.create()
+    messages: list[AnyMessage] = [UserMessage("What is the story behind the smiths song 'How Soon Is Now?'")]
+
+    guardrail = CopyrightViolationGuardrail(chat_model=chat_model)
+    result: GuardrailResult = await guardrail.evaluate(messages)
+
+    assert not result.is_harmful
