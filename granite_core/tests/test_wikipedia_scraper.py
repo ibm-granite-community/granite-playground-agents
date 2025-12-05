f61839ad017a23fd4139cd90533ca5536d04f03a
MICHAEL DESMOND <mdesmond@us.ibm.com>
2025-12-05 14:11:19 -0500

    fix: set useragent on wiki scraper test
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

--- granite_core/tests/test_wikipedia_scraper.py
+++ granite_core/tests/test_wikipedia_scraper.py
@@ -7,11 +7,13 @@ from httpx import AsyncClient
 
 from granite_core.search.scraping.types import ScrapedContent
 from granite_core.search.scraping.wikipedia import WikipediaScraper
+from granite_core.search.user_agent import UserAgent
 
 
 @pytest.mark.asyncio
 async def test_wikipedia_scraping() -> None:
     client = AsyncClient()
+    client.headers.update({"User-Agent": UserAgent().user_agent})
     scraper = WikipediaScraper()
     content: ScrapedContent | None = await scraper.ascrape(
         link="https://en.wikipedia.org/wiki/IBM",
