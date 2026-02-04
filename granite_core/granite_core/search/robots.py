f36b3631a513944843300515146b1c01176ca861
MICHAEL DESMOND <mdesmond@us.ibm.com>
2026-02-04 14:24:33 -0500

    fix: better robots logic and ttl
    Signed-off-by: MICHAEL DESMOND <mdesmond@us.ibm.com>

--- granite_core/granite_core/search/robots.py
+++ granite_core/granite_core/search/robots.py
@@ -1,37 +1,80 @@
 # © Copyright IBM Corporation 2025
 # SPDX-License-Identifier: Apache-2.0
 
+import time
 from logging import Logger
 from urllib.parse import ParseResult, urlparse
 from urllib.robotparser import RobotFileParser
 
 from httpx import AsyncClient
 from httpx._models import Response
+from pydantic import BaseModel, ConfigDict
 
 from granite_core.cache import AsyncLRUCache
 from granite_core.logging import get_logger
 
+
+class MutableRobotFileParser(RobotFileParser):
+    # Setter for disallow_all
+    def set_disallow_all(self, value: bool) -> None:
+        self.disallow_all = value
+
+    # Setter for allow_all
+    def set_allow_all(self, value: bool) -> None:
+        self.allow_all = value
+
+
+class CachedRobotParser(BaseModel):
+    parser: MutableRobotFileParser
+    timestamp: float
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
 logger: Logger = get_logger(logger_name=__name__)
-_robot_cache: AsyncLRUCache[str, RobotFileParser] = AsyncLRUCache[str, RobotFileParser](max_size=1000)
+_robot_cache: AsyncLRUCache[str, CachedRobotParser] = AsyncLRUCache[str, CachedRobotParser](max_size=500)
+CACHE_TTL = 604800  # 7 days in seconds: 7 * 24 * 60 * 60
+
+
+async def get_robots_parser(client: AsyncClient, robots_url: str, user_agent: str = "*") -> MutableRobotFileParser:
+    cached_parser: CachedRobotParser | None = await _robot_cache.get(key=robots_url)
+
+    # If nothing in cache, or what we have in cache is stale then we need to fetch it
+    if not cached_parser or (cached_parser and (time.time() - cached_parser.timestamp > CACHE_TTL)):
+        logger.info(msg=f"Need to load {robots_url}, it is not available or stale.")
+        rp: MutableRobotFileParser = MutableRobotFileParser()
+
+        try:
+            async with AsyncClient(timeout=5.0, headers={"User-Agent": user_agent}) as client:
+                response: Response = await client.get(url=robots_url, follow_redirects=True)
+
+                if response.status_code == 200:
+                    rp.parse(lines=response.text.splitlines())
+                elif response.status_code in [401, 403]:
+                    # If forbidden, robots.txt logic usually says 'disallow all'
+                    logger.info(msg=f"Robots.txt for {robots_url} is forbidden! Assuming disallowed.")
+                    rp.set_disallow_all(value=True)
+                else:
+                    # If not found, robots.txt logic usually says 'allow all'
+                    logger.info(msg=f"Robots.txt for {robots_url} is unavailable! Assuming allowed.")
+                    rp.set_allow_all(value=True)
+
+        except Exception:
+            # If robots.txt is unreachable, treat as allowed
+            logger.info(msg=f"Robots.txt for {robots_url} is unavailable! Assuming allowed.")
+            rp.set_allow_all(value=True)
+
+        cached_parser = CachedRobotParser(parser=rp, timestamp=time.time())
+        await _robot_cache.set(key=robots_url, value=cached_parser)
+
+    return cached_parser.parser
 
 
 async def can_fetch(client: AsyncClient, url: str, user_agent: str = "*") -> bool:
     parsed: ParseResult = urlparse(url)
     robots_url: str = f"{parsed.scheme}://{parsed.netloc}/robots.txt"
-    if not await _robot_cache.exists(key=robots_url):
-        rp: RobotFileParser = RobotFileParser()
-
-        try:
-            async with AsyncClient(timeout=5.0) as client:
-                response: Response = await client.get(url=robots_url, follow_redirects=True)
-                rp.parse(lines=response.text.splitlines())
-        except Exception:
-            logger.info(msg=f"Robots.txt for {robots_url} is unavailable! Assuming allowed.")
-            return True  # If robots.txt is unreachable, treat as allowed
-
-        await _robot_cache.set(key=robots_url, value=rp)
-
-    cached_rp: RobotFileParser | None = await _robot_cache.get(key=robots_url)
-    allow_fetch: bool = cached_rp.can_fetch(useragent=user_agent, url=url) if cached_rp else True
+    parser: MutableRobotFileParser = await get_robots_parser(
+        robots_url=robots_url, client=client, user_agent=user_agent
+    )
+    allow_fetch: bool = parser.can_fetch(useragent=user_agent, url=url)
     logger.info(msg=f"Robots.txt for {robots_url} {'allows' if allow_fetch else 'disallows'} fetching {url}")
     return allow_fetch
--- granite_core/tests/test_robots.py
+++ granite_core/tests/test_robots.py
@@ -2,21 +2,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
+import asyncio
+
 import httpx
 import pytest
 
-from granite_core.search.robots import can_fetch
+import granite_core.search.robots as robots
 from granite_core.search.user_agent import UserAgent
 
 
 @pytest.mark.asyncio
 async def test_robots_allowed() -> None:
     """Test basic chat infrastructure"""
-    async with httpx.AsyncClient(timeout=5.0) as client:
-        assert await can_fetch(client=client, url="https://www.ibm.com/about", user_agent=UserAgent().user_agent)
-        assert await can_fetch(client=client, url="https://www.wikipedia.com", user_agent=UserAgent().user_agent)
-        assert await can_fetch(client=client, url="https://www.netwes.com/", user_agent=UserAgent().user_agent)
-        assert await can_fetch(
+    async with httpx.AsyncClient(timeout=5) as client:
+        assert await robots.can_fetch(client=client, url="https://www.ibm.com/about", user_agent=UserAgent().user_agent)
+        assert await robots.can_fetch(client=client, url="https://www.wikipedia.com", user_agent=UserAgent().user_agent)
+        assert await robots.can_fetch(client=client, url="https://www.netwes.com/", user_agent=UserAgent().user_agent)
+        assert await robots.can_fetch(
             client=client,
             url="https://dataplatform.cloud.ibm.com/docs/content/wsj/getting-started/whats-new-wx.html?context=wx",
             user_agent=UserAgent().user_agent,
@@ -26,11 +28,44 @@ async def test_robots_allowed() -> None:
 @pytest.mark.asyncio
 async def test_robots_forbidden() -> None:
     """Test basic chat infrastructure"""
-    async with httpx.AsyncClient(timeout=5.0) as client:
-        assert not await can_fetch(client=client, url="https://facebook.com", user_agent=UserAgent().user_agent)
-        assert not await can_fetch(client=client, url="https://instagram.com", user_agent=UserAgent().user_agent)
-        assert not await can_fetch(
+    async with httpx.AsyncClient(timeout=5) as client:
+        assert not await robots.can_fetch(client=client, url="https://facebook.com", user_agent=UserAgent().user_agent)
+        assert not await robots.can_fetch(client=client, url="https://instagram.com", user_agent=UserAgent().user_agent)
+        assert not await robots.can_fetch(
             client=client,
             url="https://www.reddit.com/r/IBM/comments/1dpl799/your_opinionview_on_granite_models/",
             user_agent=UserAgent().user_agent,
         )
+
+
+@pytest.mark.asyncio
+async def test_cache_ttl() -> None:
+    """Test basic chat infrastructure"""
+    async with httpx.AsyncClient(timeout=5) as client:
+        default_cache_ttl = robots.CACHE_TTL
+
+        # Reduce cache ttl to test invalidation
+        robots.CACHE_TTL = 1
+
+        old_robots_parser: robots.MutableRobotFileParser = await robots.get_robots_parser(
+            client=client, robots_url="https://www.ibm.com/robots.txt"
+        )
+
+        await asyncio.sleep(robots.CACHE_TTL + 1)
+
+        new_robots_parser: robots.MutableRobotFileParser = await robots.get_robots_parser(
+            client=client, robots_url="https://www.ibm.com/robots.txt"
+        )
+
+        # The parser should be new
+        assert old_robots_parser != new_robots_parser
+
+        robots.CACHE_TTL = default_cache_ttl
+
+        # Should cache hit
+        cached_robots_parser: robots.MutableRobotFileParser = await robots.get_robots_parser(
+            client=client, robots_url="https://www.ibm.com/robots.txt"
+        )
+
+        # Verify cache hit
+        assert cached_robots_parser == new_robots_parser
